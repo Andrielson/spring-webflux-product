575f8edc5201b04b4f053aefc75588b14c8431d0
Andrielson Silva <andrielson@gmail.com>
2022-08-17 22:47:16 -0300

    First commit

---
+++ src/test/java/com/github/andrielson/spring/webflux/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.andrielson.spring.webflux.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
