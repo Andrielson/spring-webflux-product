83fee747c1a271435d27a55f54593d6cf2f39991
Andrielson Silva <andrielson@gmail.com>
2022-08-17 22:48:21 -0300

    Add product resource

---
+++ src/main/java/com/github/andrielson/spring/webflux/product/controller/ProductController.java
@@ -0,0 +1,50 @@
+package com.github.andrielson.spring.webflux.product.controller;
+
+import com.github.andrielson.spring.webflux.product.dto.ProductDto;
+import com.github.andrielson.spring.webflux.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("product")
+public class ProductController {
+    private final ProductService productService;
+
+    @GetMapping("all")
+    public Flux<ProductDto> all() {
+        return this.productService.getAll();
+    }
+
+    @GetMapping("price-range")
+    public Flux<ProductDto> getProductsByRange(@RequestParam("min") int min, @RequestParam("max") int max) {
+        return this.productService.getProductsByPriceRange(min, max);
+    }
+
+    @GetMapping("{id}")
+    public Mono<ResponseEntity<ProductDto>> getProductById(@PathVariable String id) {
+        return this.productService.getProductById(id)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Mono<ProductDto> insertProduct(@RequestBody Mono<ProductDto> productDtoMono) {
+        return this.productService.insertProduct(productDtoMono);
+    }
+
+    @PutMapping("{id}")
+    public Mono<ResponseEntity<ProductDto>> updateProduct(@PathVariable String id, @RequestBody Mono<ProductDto> productDtoMono) {
+        return this.productService.updateProduct(id, productDtoMono)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("{id}")
+    public Mono<Void> deleteProduct(@PathVariable String id) {
+        return this.productService.deleteProduct(id);
+    }
+}
